b4f0b2f3c15efdb40ecaa491ef0c0cf394ce77c7
mazn74793-png <mazn74793@gmail.com>
2026-02-25 14:17:46 +0200

    Create schedule-data.js with class schedule
    Added a schedule data structure for classes.

---
+++ assets/schedule-data.js
@@ -0,0 +1,9 @@
+// غيّر المواعيد هنا براحتك
+window.MF_SCHEDULE = [
+  { grade: "1 ثانوي", subject: "Math", day: "السبت", time: "4:00 م", group: "A" },
+  { grade: "1 ثانوي", subject: "English", day: "الأحد", time: "5:00 م", group: "A" },
+  { grade: "2 ثانوي", subject: "Math", day: "الاثنين", time: "6:00 م", group: "B" },
+  { grade: "2 ثانوي", subject: "English", day: "الأربعاء", time: "4:30 م", group: "B" },
+  { grade: "3 ثانوي", subject: "Math", day: "الخميس", time: "6:30 م", group: "C" },
+  { grade: "3 ثانوي", subject: "English", day: "الجمعة", time: "3:30 م", group: "C" },
+];
